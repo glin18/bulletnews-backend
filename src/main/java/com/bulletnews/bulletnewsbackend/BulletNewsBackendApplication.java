70ef69d2d9fa9db6af57548e8f8dd65c69089f6c
Gary Lin 林品辰 <garylin2018@gmail.com>
2024-02-26 23:50:59 +0800

    feat: spring initializer create app

---
+++ src/main/java/com/bulletnews/bulletnewsbackend/BulletNewsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bulletnews.bulletnewsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletNewsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BulletNewsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bulletnews/bulletnewsbackend/BulletNewsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulletnews.bulletnewsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletNewsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
