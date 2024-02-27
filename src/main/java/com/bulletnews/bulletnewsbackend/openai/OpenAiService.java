1375c078765bd066e6093c62ff7e7feb22ddf4c1
Gary Lin 林品辰 <garylin2018@gmail.com>
2024-02-27 19:03:32 +0800

    refactor: remove unused imports

--- src/main/java/com/bulletnews/bulletnewsbackend/openai/OpenAiService.java
+++ src/main/java/com/bulletnews/bulletnewsbackend/openai/OpenAiService.java
@@ -1,17 +1,13 @@
 package com.bulletnews.bulletnewsbackend.openai;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
-import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Slf4j
