090940b754da9e3106d58eebf92d8e6b4bb47917
Gary Lin 林品辰 <garylin2018@gmail.com>
2024-02-27 14:43:55 +0800

    feat: create news entity

--- src/main/java/com/bulletnews/bulletnewsbackend/news/News.java
+++ src/main/java/com/bulletnews/bulletnewsbackend/news/News.java
@@ -1,4 +1,33 @@
 package com.bulletnews.bulletnewsbackend.news;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
 public class News {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500)
+    private String title;
+
+    private String description;
+
+    private String summary;
+
+    @Column(length = 1000)
+    private String url;
+
+    @Column(length = 1000)
+    private String urlToImage;
+
+    private String publishedAt;
+
+    private String content;
+
+    private String sourceName;
+
 }
