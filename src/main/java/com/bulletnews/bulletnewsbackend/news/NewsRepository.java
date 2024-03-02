162d9ace09d883a932818e6926aa9d06af1f7a97
Gary Lin 林品辰 <garylin2018@gmail.com>
2024-03-02 14:26:34 +0800

    feat: remove unused repository method

--- src/main/java/com/bulletnews/bulletnewsbackend/news/NewsRepository.java
+++ src/main/java/com/bulletnews/bulletnewsbackend/news/NewsRepository.java
@@ -4,11 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface NewsRepository extends JpaRepository<News, Long> {
-    Optional<News> findByUrlOrTitleOrPublishedAt(String url, String title, String publishedAt);
     boolean existsByUrlOrTitleOrPublishedAt(String url, String title, String publishedAt);
     List<News> findAllByCategory_Id(Long id);
 }
