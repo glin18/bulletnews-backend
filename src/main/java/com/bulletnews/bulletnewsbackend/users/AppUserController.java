e1c6e7167687b9f814d4a910d73dde075f05b4a8
Gary Lin 林品辰 <garylin2018@gmail.com>
2024-03-06 01:39:48 +0800

    feat: add find all users

--- src/main/java/com/bulletnews/bulletnewsbackend/users/AppUserController.java
+++ src/main/java/com/bulletnews/bulletnewsbackend/users/AppUserController.java
@@ -1,7 +1,22 @@
 package com.bulletnews.bulletnewsbackend.users;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class AppUserController {
+
+    private final AppUserService appUserService;
+
+    @GetMapping("")
+    public List<AppUser> findAll(){
+        return appUserService.findAll();
+    }
+
 }
--- src/main/java/com/bulletnews/bulletnewsbackend/users/AppUserService.java
+++ src/main/java/com/bulletnews/bulletnewsbackend/users/AppUserService.java
@@ -1,7 +1,16 @@
 package com.bulletnews.bulletnewsbackend.users;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Service
 public class AppUserService {
+
+    private final AppUserRepository appUserRepository;
+    public List<AppUser> findAll() {
+        return appUserRepository.findAll();
+    }
 }
